4f1b9201ca20acc8fd56b34809024183f786fe2a
Fatema Khanom <fatemaurmi2019@gmail.com>
2024-09-06 12:47:24 +0600

    revome all chart

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// import Banner from '../../Components/Banner/Banner';
-//import Chart1 from '../../Components/Charts/Chart1';
-import Charts from '../../Components/Charts/Charts';
+
 import VideoBanner from '../../Components/Banner/Banner';
 import Map from '../../Components/Map/Map';
 
@@ -11,7 +9,7 @@ const Home = () => {
             <VideoBanner></VideoBanner>
             {/* <Banner></Banner> */}
             
-            <Charts></Charts>
+            
             <Map></Map>
         </div>
     );
